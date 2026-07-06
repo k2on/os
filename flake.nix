5fa7fb482611abf5ac84b2d13a481df7689712fb
Max Koon <contact@koon.us>
2026-07-06 13:28:14 -0500

    refactor: add den flake input

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "Koon OS";
 
   inputs = {
+    den.url = "github:denful/den";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
 
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
@@ -52,68 +53,5 @@
     apple-fonts.url= "github:Lyndeno/apple-fonts.nix";
   };
 
-
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs;} (inputs.import-tree ./modules);
-
-  # outputs = { self, nixpkgs, nixpkgs-unstable, nixos-unstable, nixos-apple-silicon, home-manager
-  #   , nixvim, sops-nix, terranix, zen-browser, apple-fonts, proton-pass-cli, firefox-addons, ... }:
-  #   let
-  #     forAllSystems = function:
-  #       nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed
-  #       (system: function nixpkgs.legacyPackages.${system});
-  #   in {
-  #
-  #     nixosConfigurations = {
-  #       max = let
-  #         system = "aarch64-linux";
-  #         pkgs-unstable = import nixpkgs-unstable { inherit system; };
-  #         secrets = import ./secrets;
-  #       in nixpkgs.lib.nixosSystem {
-  #         inherit system;
-  #         specialArgs = { inherit pkgs-unstable secrets zen-browser apple-fonts proton-pass-cli; };
-  #         modules = [
-  #           ./host/max/default.nix
-  #           nixos-apple-silicon.nixosModules.apple-silicon-support
-  #           sops-nix.nixosModules.sops
-  #           home-manager.nixosModules.home-manager
-  #           {
-  #             home-manager.useGlobalPkgs = true;
-  #             home-manager.useUserPackages = true;
-  #             home-manager.extraSpecialArgs = { inherit secrets zen-browser firefox-addons system pkgs-unstable; };
-  #             home-manager.users.max = { config, pkgs, lib, ... }: {
-  #               imports = [
-  #                 sops-nix.homeManagerModules.sops
-  #                 nixvim.homeModules.nixvim
-  #                 zen-browser.homeModules.beta
-  #                 ./host/max/home.nix # Import your home.nix here
-  #               ];
-  #             };
-  #           }
-  #         ];
-  #       };
-  #
-  #       ark = let
-  #         system = "x86_64-linux";
-  #         secrets = import ./secrets;
-  #       in nixos-unstable.lib.nixosSystem {
-  #         inherit system;
-  #         specialArgs = { inherit secrets; };
-  #         modules = [
-  #           ./host/ark/default.nix 
-  #           sops-nix.nixosModules.sops
-  #         ];
-  #       };
-  #     };
-  #
-  #     devShells = forAllSystems (pkgs: {
-  #       default = pkgs.mkShell {
-  #         packages = with pkgs; [
-  #           age
-  #           ssh-to-age
-  #           sops
-  #           just
-  #         ];
-  #       };
-  #     });
-  #   };
 }
